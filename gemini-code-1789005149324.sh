3d5db363ca0dc0ac0070c60699b81f171304203e
mplandau <mplandau24@gmail.com>
2026-09-09 21:53:09 -0400

    Add files via upload

---
+++ gemini-code-1789005149324.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: AI gas price forecaster"
+git branch -M main
+git remote add origin https://github.com/mplandau/gasai.git
+git push -u origin main
